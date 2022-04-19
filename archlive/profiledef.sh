02f60d86365f1a39c623d9f72ce5792a40c4bf29
Lucas Burlingham <lucas@lucasburlingham.me>
2022-04-18 23:04:07 -0400

    Add production and development configuration

--- archlive/profiledef.sh
+++ archlive/profiledef.sh
@@ -2,17 +2,21 @@
 # shellcheck disable=SC2034
 
 iso_name="jovarkos"
-iso_label="jovarkos-$(date +%Y.%m.%d)-x86_64"
 iso_publisher="JovarkOS <https://jovarkos.org>"
 iso_application="JovarkOS Live Installer"
 iso_version="$(date +%Y.%m.%d)"
+# Date format: YYY.MM.DD:HH:MM:SS
+iso_label="jovarkos-$(date +%Y.%m.%d-%H:%M:%S)-x86_64.iso"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+# Production
+# airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+# Development
+airootfs_image_tool_options=('-comp' 'cat' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
